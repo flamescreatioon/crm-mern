a03d7b490e06d787c84350cd35234b08ac02d61c
Ugochukwu  Nelson Achor <92747656+flamescreatioon@users.noreply.github.com>
2025-08-07 15:31:37 +0100

    first commit

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const authRoutes = require("./routes/auth");
+app.use("/api/auth", authRoutes); 
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(()=>{
+  console.log("MongoDB connected successfully");
+}).catch((err)=>{
+  console.error("MongoDB connection error:", err);
+});
+
+app.get("/", (req, res) => {
+  res.send("CRM backend is running");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
