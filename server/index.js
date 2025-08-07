10d0516ea15ce2969649adddcc892c61cca0aace
Ugochukwu  Nelson Achor <92747656+flamescreatioon@users.noreply.github.com>
2025-08-07 15:34:12 +0100

    changes

--- server/index.js
+++ server/index.js
@@ -17,8 +17,6 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URL = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 }).then(()=>{
   console.log("MongoDB connected successfully");
 }).catch((err)=>{
